03006d486e9bb693e841dba3c6233d090f8ee697
xlinayo <xeila2012@gmail.com>
2025-12-15 18:44:55 +0100

    XEILA LIÑAYO Update ej2c3.py

--- 2c/ej2c3.py
+++ 2c/ej2c3.py
@@ -76,6 +76,10 @@ def factorial(number: int):
 
 def calculate_factorial(number: int):
     # Write here your code
+    try:
+        return factorial(number)
+    except Exception as error:
+        return f"An unexpected error has occurred: {error}"
     pass
 
 # Si quieres probar tu código, descomenta las siguientes líneas y ejecuta el script
